a207ba76fe62933e1829be7c77b5efcfdcbf8046
Abdullah Al Mubin <amttawsik99@gmail.com>
2022-10-24 23:06:32 +0600

    find course by id

--- index.js
+++ index.js
@@ -5,8 +5,23 @@ app.use(cors());
 
 const Port = process.env.Port || 5000;
 
+const courses = require("./Data/course.json");
+
 app.get("/", (req, res) => {
     res.send("Now server is running");
+  });
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+    const id = req.params.id;
+    const getSingleCourse = courses?.find((c) => c.id == id);
+    if (!getSingleCourse) {
+      res.send("Porduct khuje pai nai");
+    }
+    res.send(getSingleCourse);
   });
 
 
